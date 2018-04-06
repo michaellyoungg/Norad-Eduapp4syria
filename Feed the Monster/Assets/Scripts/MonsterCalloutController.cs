942524f1d6b18d944f87ff7ab4cf105919ad471f
Eric <bookjunkie93@gmail.com>
2018-04-06 15:22:56 -0400

    edited positive feedback prefabs

--- Feed the Monster/Assets/Scripts/MonsterCalloutController.cs
+++ Feed the Monster/Assets/Scripts/MonsterCalloutController.cs
@@ -253,9 +253,7 @@ public class MonsterCalloutController : MonoBehaviour {
 			//			voice = Resources.Load ("Sounds/Voice/Letters/" + Common.Instance.GetIsolatedForm(GameplayController.Instance.CurrentSegment.MonsterRequiredLetters[0])) as AudioClip;
 			fileName = ArabicBaseForm.Extract (RTL.Clean(GameplayController.Instance.CurrentSegment.MonsterRequiredLetters [0]));
 			url = "Sounds/Voice/Letters/" + fileName;
-			#if UNITY_IOS || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
 			url= url.Normalize(NormalizationForm.FormD);
-			#endif
 			voice = Resources.Load (url) as AudioClip;
 			if (voice != null) {
 				AudioController.Instance.PlaySound (voice, 1, monster.Pitch);
@@ -270,9 +268,7 @@ public class MonsterCalloutController : MonoBehaviour {
 				word += letter;
 			}
 			url = "Sounds/Voice/Words/" + ArabicBaseForm.Extract (RTL.Clean(word));
-			#if UNITY_IOS || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
 			url= url.Normalize(NormalizationForm.FormD);
-			#endif
 			voice = Resources.Load (url) as AudioClip;
 			if (voice != null) {
 				AudioController.Instance.PlaySound (voice, 1, monster.Pitch);
