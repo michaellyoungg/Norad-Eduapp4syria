fb24eb689efecfcb66c3188500857022496cdca2
= <willowcarter42@gmail.com>
2019-04-09 10:39:07 -0400

    Amharic localization

---
+++ Feed the Monster/Assets/Plugins/Multiling/Examples/Scripts/SampleKhmer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleKhmer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("កញ្ជ្រោងពណ៌ត្នោតរហ័ស");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
